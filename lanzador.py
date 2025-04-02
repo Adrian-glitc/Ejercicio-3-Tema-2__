b7848119e51a0cce7fa04412243cb8db18d30829
Adrian-glitc <amadridm@student.universidadunie.com>
2025-04-02 17:44:30 +0200

    cambios

--- lanzador.py
+++
@@ -1,7 +0,0 @@
-from Magia_NUmerica import magia_numerica
-
-def main():
-    lista = [1, 1, 3, 4, 5, 6, 6, 7, 8, 8]
-    print(f"Lista original: {lista}")
-    resultado = magia_numerica(lista)
-    print(f"Lista procesada: {resultado}")
